84d3ea21d7c0c73fbdefde44755916055a741d2a
Michael Nobles <Mnobles33@gmail.com>
2024-09-27 16:00:50 -0700

    Final commit!

--- frontend/components/Figure.js
+++ frontend/components/Figure.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const StyledFigure = styled.figure`
+ max-width: 200px;
+
+ figcaption {
+    font-style: italic;
+ }
+`
+
 export default function Figure({ imageURL, caption }) {
     return (
-        <figure>
+        <StyledFigure>
             <img src={imageURL} />
             <figcaption>My favorite city taken on {caption}</figcaption>
-        </figure>
+        </StyledFigure>
     )
 }
